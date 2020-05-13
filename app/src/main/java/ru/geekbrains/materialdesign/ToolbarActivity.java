c73a9c69e01105ce8286ecdcc74c9fbb5a2ef126
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-05-13 20:21:00 +0300

    Добавлены 2 вида прогресс-баров и возможность их скрывать/показывать нажатиями кнопок

--- app/src/main/java/ru/geekbrains/materialdesign/ToolbarActivity.java
+++ app/src/main/java/ru/geekbrains/materialdesign/ToolbarActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class ToolbarActivity extends AppCompatActivity {
+
+    private ProgressBar progressBarRound;
+    private ProgressBar progressBarLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,17 @@ public class ToolbarActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setLogo(R.mipmap.ic_launcher);
 
+        progressBarRound = findViewById(R.id.progress_round_activity_progress);
+        progressBarLine = findViewById(R.id.progress_line_activity_progress);
+    }
+
+    public void show(View view) {
+        progressBarLine.setVisibility(View.VISIBLE);
+        progressBarRound.setVisibility(View.VISIBLE);
+    }
+
+    public void hide(View view) {
+        progressBarLine.setVisibility(View.INVISIBLE);
+        progressBarRound.setVisibility(View.INVISIBLE);
     }
 }
